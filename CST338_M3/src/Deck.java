d597866e2b7388138f3d9d03d1e8b6491c371d0c
kateisamonster <49127975+kateisamonster@users.noreply.github.com>
2019-05-19 14:35:36 -0400

    putting previous work into repo

---
+++ CST338_M3/src/Deck.java
@@ -0,0 +1,86 @@
+class Deck
+{
+   // Max number of card in 6 decks.  6 * 52 = 312
+   public final int MAX_CARDS = 312;
+   
+   private static Card[] masterPack = new Card[52];
+   
+   private Card[] cards = new Card[MAX_CARDS];
+   private int topCard;
+   private int cardsInDeck;
+   
+   Deck()
+   {
+      if (masterPack[0] != new Card('2', Card.Suit.SPADES))
+      {
+         allocateMasterPack();
+      }
+      cardsInDeck = 52;
+      
+      //TODO  populate cards array with shuffled cards
+      
+   }
+   
+   Deck(int numPacks)
+   {
+      if (masterPack[0] != new Card('2', Card.Suit.SPADES))
+      {
+         allocateMasterPack();
+      }
+      cardsInDeck = 52 * numPacks;
+      
+    //TODO  populate cards array with shuffled cards
+      
+   }
+   
+   private void allocateMasterPack()
+   {
+      char[] values = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
+      int arrayIndex = 0;
+      for(char v:values)
+      {
+         for(int s = 0; s < 4; s++)
+         {
+            Card.Suit newSuit = getSuit(s);
+            this.cards[arrayIndex] = new Card(v, newSuit);
+            arrayIndex++;
+         }
+      }
+   }
+   
+   private Card.Suit getSuit(int num)
+   {
+      Card.Suit suit  = Card.Suit.SPADES;
+      
+      switch(num) { 
+         
+         case 0:
+            suit = Card.Suit.SPADES;
+            break;
+         case 1:
+            suit = Card.Suit.HEARTS;
+            break;
+         case 2:
+            suit = Card.Suit.CLUBS;
+            break;
+         case 3:
+            suit = Card.Suit.DIAMONDS;
+            break;
+         default:
+            //Included for degugging
+            System.out.println("Encountered error");
+      }   
+      return suit;
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+}
---
+++ CST338_M3/src/Main.java
@@ -0,0 +1,88 @@
+class Main
+{
+   public static void main(String[] args)
+   {
+      //Card class testing
+      Card card1 = new Card();
+      Card card2 = new Card('d', Card.Suit.CLUBS);
+      Card card3 = new Card(card1);
+
+      System.out.println(card1);
+      System.out.println(card2);
+      System.out.println(card3);
+
+      if (card1.equals(card3))
+      {
+         System.out.println("equals function works");
+      }
+
+      card1.set('z', Card.Suit.DIAMONDS);
+      card2.set('K', Card.Suit.SPADES);
+
+      System.out.println(card1);
+      System.out.println(card2);
+      System.out.println(card3);
+
+      System.out.println(card2.getValue());
+      System.out.println(card2.getSuit());
+      System.out.println(card2.getErrorFlag());
+
+      System.out.println(card1.getValue());
+      System.out.println(card1.getSuit());
+      System.out.println(card1.getErrorFlag());
+      
+      //Hand class testing
+      // Testing for Hand Class
+      // Create new cards
+      Card meow = new Card('9',Card.Suit.SPADES);
+      Card meow2 = new Card('J', Card.Suit.DIAMONDS);
+      Card meow3 = new Card('A', Card.Suit.HEARTS);
+      
+      Hand hand = new Hand();
+
+      //Add cards into hand until full
+      boolean checkFull = true;
+      while(checkFull == true)
+      {
+          checkFull = hand.takeCard(meow);
+          if (checkFull == false)
+          { 
+              
+              break;
+          }
+          checkFull = hand.takeCard(meow2);
+          if (checkFull == false)
+          { 
+              break;
+          }
+          checkFull = hand.takeCard(meow3);
+      }
+      System.out.println("Hand full\nAfter deal");
+      System.out.printf("Hand = ( %s )%n%n", hand.toString());
+      
+      // Inspect Cards
+      int i ;
+      System.out.println("Testing inspectCard()");
+      Card testCard = new Card();
+      
+      //Valid Card
+      testCard.set('9', Card.Suit.CLUBS);
+      System.out.println(testCard.toString());
+      
+      //Illegal Card
+      testCard.set('z', Card.Suit.CLUBS);
+      System.out.println(testCard.toString());
+      
+      // Display all hands.
+      for(i = 0 ; i <= hand.getNumCards()-1; i ++ )
+      {
+          testCard = hand.inspectCard(i);
+          System.out.printf("Playing %s%n", testCard.toString());     
+      }
+          
+      hand.resetHand();
+      System.out.println("\nAfter playing all cards");
+      System.out.printf("Hand = ( %s )%n%n", hand.toString());
+   }
+}
+
