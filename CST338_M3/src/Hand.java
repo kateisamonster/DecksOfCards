7b1a5f00fbe1f72abb5937398a4788c7b2da348c
Kate <49127975+kateisamonster@users.noreply.github.com>
2019-05-20 18:37:45 -0400

    debugging and more complete documentation

--- CST338_M3/src/Hand.java
+++ CST338_M3/src/Hand.java
@@ -8,53 +8,47 @@
  * @author Michael Garber
  */
 
-class Hand {
+public class Hand {
    private Card[] myCards;
    private int numCards;
    private static final int MAX_CARDS = 52; 	// Instruction mentions public int value
    
-   // Default Constructor.
+   /** Default Constructor */
    Hand()
    {
       this.myCards = new Card[MAX_CARDS];
       numCards = 0;
    }
    
-   //Clears the current hand
+   /** Clears the current hand */
    public void resetHand()
    {
-       /*  If you just set numCards to zero.  The next time you draw a card 
-       will start to overwrite what is there.  You dont actually have to 
-       empty the array.
-       //Iterate through myCards and make each item null
-        
-       int i;
-       for (i = 0 ; i <= numCards-1 ; i++)
-       {
-           this.myCards[i] = null;
-       }
-       */
        this.numCards = 0;
    }
    
-   //adds card to the myCard array
-   public  boolean takeCard(Card card)
+   /** adds copy of a card to the hand 
+    * @param card    Card to be added to hand
+    * @return        whether a card was added or not
+    */
+   public boolean takeCard(Card card)
    {
        Card tempCard = new Card(card);
-       //tempCard = card;
-       boolean wasTaken = false;
-       if (numCards <= MAX_CARDS-1)
+       if (numCards < MAX_CARDS)
        {
-           this.myCards[this.numCards] = tempCard;
-           numCards++;
+           this.myCards[this.numCards++] = tempCard;
            return true;
        }
-       
-       return wasTaken;
-       
+       else
+       {
+           return false;
+       }
+
    }
    
-   //returns card at the highest index
+   /** plays and removes the top card in hand 
+    * @return  Card at the highest index 
+    *          (or invalid Card if empty hand)
+    */
    public Card playCard()
    {   
        Card tempCard = new Card('Z', Card.Suit.SPADES);
@@ -67,47 +61,57 @@ class Hand {
        return tempCard;
    }
    
-   // Returns all cards in the myCards Array
+   /** 
+    * @return   string representation of all cards in hand
+                formatted like "A of spades, 2 of clubs, J of hearts"
+    */ 
    public String toString()
    {
-       String tempCards = "";
-       
-       for(int i = 0; i < numCards; i++)
-       {
-           tempCards += myCards[i].toString() + ", ";
-       }
-       return tempCards;
-   }
-   
-   // mutator the numCards
-   public boolean setNumCards(int inputNumCards)
+    if(numCards==0)
+        return "";
+
+    String tempCards = "";
+    for(int i = 0; i < numCards; i++)
+       tempCards += myCards[i].toString() + ", ";
+
+    return tempCards.substring(0, tempCards.length()-2); //removes last ", "
+    }
+
+
+   /** Changes the number of cards in hand
+    * 
+    * @param inputNumCards    new value for the number of cards in hand
+    * @return
+    */
+/* //XXX I don't see setNumCards in the assignment specs & we don't use it anywhere
+ * can we delete it? -ka
+   public boolean setNumCards(int inputNumCards) 
    {
-       this.numCards = inputNumCards;
-       return true;
+      this.numCards = inputNumCards;
+      return true;
    }
+// */
    
-   // Accessor for numCards
+   /** Accessor for numCards
+    * 
+    * @return  number of cards in hand
+    */
    public int getNumCards()
    {
        return this.numCards;
    }
    
-   // Inspect the card at the hand position.
+   /** Inspect the card at the hand position.
+    * 
+    * @param k index of card to inspect in hand
+    * @return  card at index k
+    *          (or invalid card if k is invalid)
+    */
    public Card inspectCard(int k)
    {
-       Card tempCard = new Card();
-       
-       // Check to see if k is bad.  If it is, return errorFlag = True
-       if(k >= this.numCards || k < 0)
-       {
-           // Create a bad card that will have an errorFlag.
-           tempCard.set('z',Card.Suit.SPADES);
-       }
-       //Else Return card
-       else
-       {
-           tempCard = this.myCards[k];
-       }
-       return tempCard;
+       if(k >= this.numCards || k < 0) //invalid k 
+           return new Card('z',Card.Suit.SPADES); //invalid card w/ error flag
+       else 
+           return this.myCards[k];
    }
 }
